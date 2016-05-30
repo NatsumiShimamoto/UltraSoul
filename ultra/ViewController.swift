ba44c62b3326592e9949554b4ab5d1c5f73f4531
NatsumiShimamoto <nshimamoto725@gmail.com>
2016-05-30 21:44:15 +0900

    Initial Commit

---
+++ ultra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ultra
+//
+//  Created by 嶋本夏海 on 2016/05/30.
+//  Copyright © 2016年 NatsumiShimamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
